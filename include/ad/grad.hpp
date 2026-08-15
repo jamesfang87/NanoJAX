0f93dc5a27e9f50b5b32342b018dccc1b5d2ef7c
jamesfang87 <james@LAPTOP-R2B8RKOA.localdomain>
2026-08-15 02:27:17 -0500

    Feat: calculate Hessians for one variable in a multi-var function

--- include/ad/grad.hpp
+++ include/ad/grad.hpp
@@ -1,18 +1,24 @@
 #include "include/ad/scalar.hpp"
 #include "include/ad/trace.hpp"
 
+#include <cstddef>
 #include <memory>
 #include <tuple>
 #include <type_traits>
 
 namespace {
-template <typename Out> Variable ensure_scalar_out(Out &&out, Trace &tr) {
+template <typename Out>
+Variable ensure_scalar_out(Out &&out, Trace &tr, size_t argnum) {
   using T = std::decay_t<Out>;
 
   if constexpr (std::is_same_v<T, Variable>) {
     return out;
   } else if constexpr (std::is_same_v<T, Real> || std::is_same_v<T, Scalar>) {
     return lift(out, tr);
+  } else if constexpr (std::is_same_v<T, std::vector<Scalar>>) {
+    assert(argnum < out.size() &&
+           "grad: argnums is out of range for f's return vector");
+    return ensure_scalar_out(out[argnum], tr, argnum);
   } else {
     static_assert(
         false, "grad() requires f to return a scalar value. this means that a "
@@ -22,20 +28,16 @@ template <typename Out> Variable ensure_scalar_out(Out &&out, Trace &tr) {
 
 } // namespace
 
-template <typename F> auto grad(F &&f) {
-  return [f = std::forward<F>(f)](auto... inputs) {
+template <typename F> auto grad(F &&f, size_t argnums = 0) {
+  return [f = std::forward<F>(f), argnums](auto... inputs) {
     trace_stack.push(std::make_unique<Trace>());
     Trace &tr = trace();
 
     auto x = std::make_tuple(tr.add_variable(Scalar{inputs}, nullptr)...);
     auto out = std::apply(f, x);
 
-    // To allow for grad(grad(f)), f must be a single-variable function
-    // and output a scalar value
-    tr.backward(ensure_scalar_out(out, tr));
+    tr.backward(ensure_scalar_out(out, tr, argnums));
     if constexpr (sizeof...(inputs) == 1) {
-      // this is so ensure_scalar_out works, since it wouldn't work if
-      // std::vector<Scalar> was returned
       Scalar g =
           std::apply([&](const auto &x) { return tr.adjoints[x.id]; }, x);
       trace_stack.pop();
